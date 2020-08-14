f46015c2271dcea8d1381bef717d1bcff0f585f8
Renan-96 <renan.salcedo@virginiogomez.cl>
2020-08-14 18:11:58 -0400

    se agrego imagen

--- Adaptacion_certamen_2/Ejercicio_4/Program.cs
+++ Adaptacion_certamen_2/Ejercicio_4/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿    using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,24 @@ namespace Ejercicio_4
     {
         static void Main(string[] args)
         {
+            //Solicite al usuario el ingreso de una palabra, deberá mostrar la palabra reemplazando las
+            //vocales con el numero 0 ejemplo: Claudio Resultado Cl00d00
+
+            string nombre = "";
+
+            Console.WriteLine("Ingrese una palabra");
+            nombre = Console.ReadLine();                    
+            Console.WriteLine(convertir(nombre));
+
+            Console.ReadLine();
+        }
+
+        static string convertir(string nombre)
+        {
+            nombre = nombre.Replace("a", "0").Replace("e", "0").Replace("i", "0").Replace("o", "0").Replace("u", "0");
+            nombre = nombre.Replace("A", "0").Replace("E", "0").Replace("I", "0").Replace("O", "0").Replace("U", "0");
+
+            return nombre;
         }
     }
 }
--- Adaptacion_certamen_2/ejercicio_2/Program.cs
+++ Adaptacion_certamen_2/ejercicio_2/Program.cs
@@ -10,6 +10,97 @@ namespace ejercicio_2
     {
         static void Main(string[] args)
         {
+            int contador = 0, limite = 0, numero = 0;
+            int pares = 0, impares = 0, positivos = 0, negativos = 0, primos = 0, nprimos = 0;
+            int sumaP = 0, sumaI = 0;
+
+            try
+            {
+                Console.WriteLine("Ingrese la cantidad de numeros");
+                limite = int.Parse(Console.ReadLine());
+
+                while (limite != contador)
+                {
+                    contador++;
+                    Console.WriteLine("ingrese un numero");
+                    numero = int.Parse(Console.ReadLine());
+
+                    if (numero % 2 == 0)
+                    {
+                        sumaP = numero + sumaP;
+                        pares++;
+                    }
+                    else
+                    {
+                        sumaI = numero + sumaI;
+                        impares++;
+                    }
+                    if (numero >= 0)
+                    {
+                        positivos++;
+                    }
+                    else
+                    {
+                        negativos++;
+                    }
+                    if (numero > 1)
+                    {
+                        if (numero == 2 || numero == 3)
+                        {
+                            primos++;
+                        }
+                        if (numero % 2 == 0)
+                        {
+                            nprimos++;
+                        }
+                        else
+                        {
+                            if (numero % 3 == 0)
+                            {
+                                nprimos++;
+                            }
+                            else
+                            {
+                                if (numero / 1 == numero)
+                                {
+                                    if (numero / numero == 1)
+                                    {
+                                        primos = primos + 1;
+                                    }
+
+                                }
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Los numeros pares son: " + pares);
+                Console.WriteLine();
+                Console.WriteLine("Los numeros impares son: " + impares);
+                Console.WriteLine();
+                Console.WriteLine("Los numeros positivos son: " + positivos);
+                Console.WriteLine();
+                Console.WriteLine("Los numeros negativos son: " + negativos);
+                Console.WriteLine();
+                Console.WriteLine("La suma de los numeros pares son: " + sumaP);
+                Console.WriteLine();
+                Console.WriteLine("La suma de los numeros impares son: " + sumaI);
+                Console.WriteLine();
+                Console.WriteLine("Los numeros primos son: " + primos);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Solo ingresar números por favor.");
+                Console.WriteLine();
+                Console.WriteLine("Error: " + e.Message);
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.ReadLine();
         }
     }
 }
